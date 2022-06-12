a6369086db6a7ee5433013852edf14992ea4e72e
MariiaSl <mvs030915@gmail.com>
2022-06-12 20:17:09 +0300

    lesson 49 task3 fixed

--- react49/task3/src/Comment.jsx
+++ react49/task3/src/Comment.jsx
@@ -5,14 +5,14 @@ import UserInfo from './UserInfo';
 
 const formatDate = (date) => moment(date).format('DD MMM YYYY');
 
-function Comment(props) {
+const Comment = ({ user, text, date }) => {
   return (
     <div className="comment">
-      <UserInfo user={props.user} />
-      <div className="comment__text">{props.text}</div>
-      <div className="comment__date">{formatDate(props.date)}</div>
+      <UserInfo user={user} />
+      <div className="comment__text">{text}</div>
+      <div className="comment__date">{formatDate(date)}</div>
     </div>
   );
-}
+};
 
 export default Comment;
