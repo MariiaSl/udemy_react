e72259a941a6aef84a81d5fc29d7c08f7c3b28ed
MariiaSl <mvs030915@gmail.com>
2022-07-04 19:43:08 +0300

    eslint rule fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,9 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/prop-types': 'off',
-    'react/function-component-definition': [
-      (2, { namedComponents: 'arrow-function' }),
-    ],
+    // 'react/function-component-definition': [
+    //   (2, { namedComponents: 'arrow-function' }),
+    // ],
+    'react/function-component-definition': 0,
   },
 };
